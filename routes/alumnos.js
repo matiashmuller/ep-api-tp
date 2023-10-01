fad93c3bd1e18dfc4fc2dbc5352f87a87dfc1a81
Matías <mullermatiash@gmail.com>
2023-10-01 19:43:47 -0300

    Trabajando en paginación (alumnos)

--- routes/alumnos.js
+++ routes/alumnos.js
@@ -3,24 +3,41 @@ var router = express.Router();
 var models = require("../models");
 
 router.get("/", (req, res) => {
-  console.log("Esto es un mensaje para ver en consola");
+  /*
+  Toma de parámetros para paginación:
+  Toma los valores paginaActual y cantidadAVer pasados como parámetros y
+  asiga un valor por defecto a cada uno (0 y 5 respectivamente), a usarse
+  si no se pasara ningún parámetro.
+  */
+  const { paginaActual = 0, cantidadAVer = 5} = req.query;
+
   models.alumno
-    .findAll({
+    .findAndCountAll({
       attributes: ["id", "dni", "nombre", "apellido", "fecha_nac"],
       //Asocicación
       include: [
         {
           as: 'carreraQueEstudia', 
-          model:models.carrera, 
+          model: models.carrera, 
           attributes: ["nombre"]
         },
         {
           as: 'materiasQueCursa', 
-          model:models.materia, 
+          model: models.materia, 
           attributes: ["nombre", "carga_horaria"],
           through: { attributes: ["id"] }
         }
-      ]
+      ],
+      /*
+      Paginación: Se muestran cantidadAVer (5 por defecto) elementos por página,
+      a partir de la página actual.
+      Ejemplo:
+        Página 0 → Elementos 1 al 5
+        Pagina 1 → Elementos 6 al 10
+      El + parsea a número los valores tomados de los parámetros.
+      */
+      limit: +cantidadAVer,
+      offset: (+paginaActual) * (+cantidadAVer)
     })
     .then(alumnos => res.send(alumnos))
     .catch(() => res.sendStatus(500));
