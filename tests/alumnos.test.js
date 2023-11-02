186f6ead44aec7a1936ef51ff9c5d0f6ea78badf
Matías <mullermatiash@gmail.com>
2023-11-01 21:49:45 -0300

    Trabajando en tests: añadido @types/jest para intellisense, NODE_ENV para bypass token en prueba, primeros test funcionales

--- tests/alumnos.test.js
+++ tests/alumnos.test.js
@@ -1,10 +1,45 @@
 const app = require('../app');
 const request = require('supertest');
+const modelos = require('../models');
+//Workaround para problema de timeout
+jest.useFakeTimers()
+//Crea una función mock de modelos
+jest.mock('../models');
 
-describe('GET /task', () => {
+describe('GET ALL /alum', () => {
+    const alumno = modelos.alumno
 
-    test('debería responder código 200', async () => {
-        const response = await request(app).get('/task').send();
-        expect(response.statusCode).toBe(404);
+    test('debería mostrar todos los alumnos contados y paginados', async () => {
+        //Envía la respuesta necesaria a partir del mock de findAndCountAll
+        await alumno.findAndCountAll.mockImplementation(() => ({
+            rows: [{
+                id: 1,
+                nombre: 'Jónico',
+                apellido: 'Dórico',
+                fecha_nac: '2000-07-25',
+                id_carrera: 1
+            }, {
+                id: 2,
+                nombre: 'Cariátides',
+                apellido: 'Magencio',
+                fecha_nac: '1998-02-19',
+                id_carrera: 3
+            }, {
+                id: 3,
+                nombre: 'Tito',
+                apellido: 'Corintio',
+                fecha_nac: '1990-10-30',
+                id_carrera: 2
+            }],
+            count: 3,
+        }));
+
+        //Hace la petición HTTP
+        const response = await request(app).get('/alum');
+
+        //Comprueba la corrección de los resultados
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeInstanceOf(Object);
+        expect(response.body).toHaveProperty('elementos')
     })
-})
+});
