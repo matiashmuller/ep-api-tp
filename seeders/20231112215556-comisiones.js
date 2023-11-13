f4794217db77ddc36ffe344abd2f348245c1e364
Matías <mullermatiash@gmail.com>
2023-11-13 00:26:48 -0300

    Agregado seed comisiones (al menos una por materia)

--- seeders/20231112215556-comisiones.js
+++ seeders/20231112215556-comisiones.js
@@ -2,16 +2,82 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('comisions', [
+      {
+        letra: "A",
+        dias: "Sábado",
+        turno: "Mañana",
+        id_materia: 1, //Prob y estad (lic)
+        id_docente: 1, //Guillén
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Martes",
+        turno: "Noche",
+        id_materia: 2, //Obj III (lic)
+        id_docente: 2, //Pizarro
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Martes",
+        turno: "Noche",
+        id_materia: 3, //taller marcado (prog)
+        id_docente: 3, //Hoyos
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Miércoles",
+        turno: "Noche",
+        id_materia: 4, //prog estructurada (prog)
+        id_docente: 2, //Pizarro
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Viernes",
+        turno: "Noche",
+        id_materia: 5, //int comandos (redes)
+        id_docente: 4, //Bianchi
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Jueves",
+        turno: "Noche",
+        id_materia: 6, //sist comunicacion (redes)
+        id_docente: 5, //Sotelo
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Miércoles",
+        turno: "Noche",
+        id_materia: 7, //EP (prog y lic)
+        id_docente: 6, //Rubio
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        letra: "A",
+        dias: "Lunes",
+        turno: "Noche",
+        id_materia: 8, //sist operativos (redes y lic)
+        id_docente: 5, //Sotelo
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        letra: "A",
+        dias: "Jueves",
+        turno: "Noche",
+        id_materia: 9, //bases de datos (redes, prog y lic)
+        id_docente: 6, //Rubio
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
